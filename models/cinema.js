03eeb11b931a54d627b7d3366ccad3edf816731b
Kaspars Dzerins <kadzer@gmail.com>
2018-08-22 15:08:50 +0100

    passing first test

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,11 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.listFilms = function() {
+  let filmTitles = [];
+    this.films.forEach(function(film){
+    filmTitles.push(film.title);
+})
+  return filmTitles;
+};
 module.exports = Cinema;
