3b252caedea3a6203d7aba39be315ba3074169e2
Kaspars Dzerins <kadzer@gmail.com>
2018-08-22 15:33:43 +0100

    passing second test

--- models/cinema.js
+++ models/cinema.js
@@ -4,9 +4,18 @@ const Cinema = function (films) {
 
 Cinema.prototype.listFilms = function() {
   let filmTitles = [];
-    this.films.forEach(function(film){
+  this.films.forEach(function(film){
     filmTitles.push(film.title);
-})
+  })
   return filmTitles;
 };
+
+Cinema.prototype.findFilm = function(title) {
+  let searchFunction = function(film){
+    return film.title === title
+  }
+  let result = this.films.find(searchFunction);
+  return result;
+};
+
 module.exports = Cinema;
